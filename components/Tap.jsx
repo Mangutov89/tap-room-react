d4cd19f0b45ba5e10c511de25ab909f9b3ed8518
lm <leo.mangutov@gmail.com>
2019-04-19 11:42:27 -0700

    add props to tap component

--- components/Tap.jsx
+++ components/Tap.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Tap(){
   return (
-      <div>
-        <h3>3A - Thato & Haley</h3>
-        <p><em>Firebase entries not saving!</em></p>
-        <hr/>
-      </div>
+    <div>
+      <h3>{props.name}</h3>
+      <h4>Brewed in: {props.brewery}</h4>
+      <p>Alcohol Percent: {props.alcoholPercent}</p>
+      <p>Brewed from this region: {props.region}</p>
+      <hr/>
+    </div>
    );
 }
+
+Tap.propTypes = {
+  name: PropTypes.string,
+  brewery: PropTypes.string,
+  alcoholPercent: PropTypes.string,
+  region: PropTypes.string
+};
 
 export default Tap;
